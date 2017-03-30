65a51e503601b7e7c7b65cc1266c7c4f8b47c7b3
Florian Walbroel <walbroel@silexica.com>
2017-03-30 12:11:20 +0200

    added temporary option to specify todo file via command line

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <ncurses.h>
 
@@ -15,20 +16,33 @@ int main(int argc, char * argv[]) {
   char * path;
   const char * homedir;
 
+  //TODO
   homedir = get_home_directory();
   if(!homedir) {
     std::cerr << "error: could not get home directory" << std::endl;
     return -1;
   }
+
   path = get_path(homedir, todo::list::default_filename.c_str());
   if(!path) {
     std::cerr << "error: could not get path to file: " << todo::list::default_filename
               << " in " << homedir << std::endl;
     return -1;
   }
+
   if(!file_exists(path)) {
+    //TODO
     std::cerr << "file: " << path << " does not exist, TODO create new" << std::endl;
     todo::list::current_file = path;
+  }
+
+  if (argc == 2) {
+    if (!file_exists(argv[1])) {
+      std::cout << argv[0] << ": can not open: " << argv[1] << std::endl;
+      return -1;
+    }
+
+    path = strdup(argv[1]);
   }
 
   todo::gui& gui = todo::gui::get();
